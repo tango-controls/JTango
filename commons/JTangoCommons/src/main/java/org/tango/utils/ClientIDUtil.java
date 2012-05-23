4928aad065d3f7dc5c9422afeee60ff6da43436d
qa-soleil <qa-soleil@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2012-05-23 11:17:38 +0000

    New Import : 0022681 : JTangoCommons is now a true opensource project

---
+++ commons/JTangoCommons/src/main/java/org/tango/utils/ClientIDUtil.java
@@ -0,0 +1,84 @@
+package org.tango.utils;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+import org.jacorb.orb.iiop.IIOPConnection;
+import org.jacorb.orb.iiop.IIOPLoopbackConnection;
+import org.omg.ETF.Connection;
+
+import fr.esrf.Tango.ClntIdent;
+import fr.esrf.Tango.LockerLanguage;
+
+/**
+ * Utility for client informations
+ * 
+ * @author ABEILLE
+ * 
+ */
+public final class ClientIDUtil {
+
+    private ClientIDUtil() {
+
+    }
+
+    /**
+     * Compare if two {@link ClntIdent} are coming from the same client
+     * 
+     * @param ident1
+     *            The first ident
+     * @param ident2
+     *            The second ident
+     * @return
+     */
+    public static boolean clientIdentEqual(final ClntIdent ident1, final ClntIdent ident2) {
+	boolean areEqual = true;
+	if (ident1 == null || ident2 == null || ident1.discriminator() == null || ident2.discriminator() == null) {
+	    areEqual = false;
+	} else if (ident1.discriminator().value() != ident2.discriminator().value()) {
+	    areEqual = false;
+	} else if (ident1.discriminator().equals(LockerLanguage.CPP) && ident1.cpp_clnt() != ident2.cpp_clnt()) {
+	    areEqual = false;
+	} else if (ident1.discriminator().equals(LockerLanguage.JAVA)
+		&& ident1.java_clnt().MainClass.equalsIgnoreCase(ident2.java_clnt().MainClass)) {
+	    areEqual = false;
+	} else if (ident1.discriminator().equals(LockerLanguage.JAVA)
+		&& !Arrays.equals(ident1.java_clnt().uuid, ident2.java_clnt().uuid)) {
+	    areEqual = false;
+	}
+	return areEqual;
+    }
+
+    public static String toString(final ClntIdent ident) {
+	final StringBuilder sb = new StringBuilder();
+	if (ident == null || ident.discriminator() == null) {
+	    sb.append("empty ClntIdent");
+	} else if (ident.discriminator().equals(LockerLanguage.CPP)) {
+	    sb.append("CPP client with PID ").append(ident.cpp_clnt());
+	} else {
+	    sb.append("Java client with Main class ").append(ident.java_clnt().MainClass);
+	}
+	return sb.toString();
+    }
+
+    /**
+     * Get host name from a {@link Connection}
+     * 
+     * @param connection
+     * @return
+     */
+    public static String hostNameFrom(final Connection connection) {
+	if (connection instanceof IIOPConnection) {
+	    final IIOPConnection iiopConnection = (IIOPConnection) connection;
+	    return iiopConnection.getSocket().getInetAddress().getHostName();
+	} else if (connection instanceof IIOPLoopbackConnection) {
+	    try {
+		return InetAddress.getLocalHost().getCanonicalHostName();
+	    } catch (final UnknownHostException e) {
+		return "localhost";
+	    }
+	}
+	throw new IllegalStateException("Unknown connection type: " + connection.getClass());
+    }
+}
