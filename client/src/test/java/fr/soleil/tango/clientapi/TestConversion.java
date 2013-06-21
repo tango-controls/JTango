08f26b75b63aeac1477ed3ca9b3ea8c00d2c2275
Gwenaelle Abeille <gwenaelle.abeille@synchrotron-soleil.fr>
2013-06-21 14:00:14 +0000

    fix test

--- client/src/test/java/fr/soleil/tango/clientapi/TestConversion.java
+++ client/src/test/java/fr/soleil/tango/clientapi/TestConversion.java
@@ -26,47 +26,47 @@ public class TestConversion {
 
     @BeforeClass
     public static void setUp() {
-	final DefaultConverters conv = new DefaultConverters();
-	conv.addConverter(new DevStateToObjectConverter());
-	conv.addConverter(new ObjectToDevStateConverter());
-	conv.addConverter(new ObjectToBooleanConverter());
-	conv.addConverter(new ObjectToNumberConverter());
-	conv.addConverter(1, new DevEncodedConverter());
-	transmorph = new Transmorph(conv);
+        final DefaultConverters conv = new DefaultConverters();
+        conv.addConverter(new DevStateToObjectConverter());
+        conv.addConverter(new ObjectToDevStateConverter());
+        conv.addConverter(new ObjectToBooleanConverter());
+        conv.addConverter(new ObjectToNumberConverter());
+        conv.addConverter(1, new DevEncodedConverter());
+        transmorph = new Transmorph(conv);
     }
 
     @Test
     public void testBoolean() throws ConverterException {
-	assertThat(transmorph.convert(10.3, boolean.class), equalTo(true));
-	assertThat(transmorph.convert("fzefef", boolean.class), equalTo(false));
-	assertThat(transmorph.convert("0", boolean.class), equalTo(false));
-	assertThat(transmorph.convert("", boolean.class), equalTo(false));
+        assertThat(transmorph.convert(10.3, boolean.class), equalTo(true));
+        assertThat(transmorph.convert("fzefef", boolean.class), equalTo(false));
+        assertThat(transmorph.convert("0", boolean.class), equalTo(false));
+        assertThat(transmorph.convert("", boolean.class), equalTo(false));
     }
 
     @Test
     public void testInt() throws ConverterException {
-	assertThat(transmorph.convert(10.3, int.class), equalTo(10));
-	assertThat(transmorph.convert(false, int.class), equalTo(0));
-	assertThat(transmorph.convert("true", int.class), equalTo(1));
-	assertThat(transmorph.convert("", int.class), equalTo(0));
+        assertThat(transmorph.convert(10.3, int.class), equalTo(10));
+        assertThat(transmorph.convert(false, int.class), equalTo(0));
+        assertThat(transmorph.convert("true", int.class), equalTo(1));
+        assertThat(transmorph.convert("", int.class), equalTo(0));
     }
 
     @Test
     public void testBig() throws ConverterException {
-	assertThat(transmorph.convert("10.3", BigDecimal.class), equalTo(new BigDecimal(10.3)));
-	assertThat(transmorph.convert("10.3", BigInteger.class), equalTo(new BigInteger("10")));
+        assertThat(transmorph.convert("10.3", BigDecimal.class), equalTo(new BigDecimal("10.3")));
+        assertThat(transmorph.convert("10.3", BigInteger.class), equalTo(new BigInteger("10")));
     }
 
     @Test
     public void testState() throws ConverterException {
-	assertThat(transmorph.convert(DevState.RUNNING, int.class), equalTo(10));
-	assertThat(transmorph.convert(DevState.RUNNING, String.class), equalTo("RUNNING"));
-	assertThat(transmorph.convert(10, DevState.class), equalTo(DevState.RUNNING));
+        assertThat(transmorph.convert(DevState.RUNNING, int.class), equalTo(10));
+        assertThat(transmorph.convert(DevState.RUNNING, String.class), equalTo("RUNNING"));
+        assertThat(transmorph.convert(10, DevState.class), equalTo(DevState.RUNNING));
     }
 
     @Test
     public void testArray() throws ConverterException {
-	assertThat(transmorph.convert(new String[][] { { "0", "2.1" }, { "0.8", "25.1" } }, short[][].class),
-		equalTo(new short[][] { { 0, 2 }, { 0, 25 } }));
+        assertThat(transmorph.convert(new String[][] { { "0", "2.1" }, { "0.8", "25.1" } }, short[][].class),
+                equalTo(new short[][] { { 0, 2 }, { 0, 25 } }));
     }
 }
