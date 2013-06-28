670e6d83940426fede121dc3873cc2be64540d12
pascal_verdier <pascal_verdier@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2013-06-28 07:19:37 +0000

    Class TangoEventListenerList replace javax.swing.event.EventListenerList

--- tangorb/common/src/fr/esrf/TangoApi/events/TangoPeriodic.java
+++ tangorb/common/src/fr/esrf/TangoApi/events/TangoPeriodic.java
@@ -38,6 +38,8 @@ import fr.esrf.Tango.DevFailed;
 import fr.esrf.TangoApi.DeviceProxy;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.EventListener;
 
 /**
  *
@@ -75,7 +77,7 @@ public class TangoPeriodic extends EventDispatcher implements java.io.Serializab
                 throws DevFailed
     {
         event_listeners.remove(ITangoPeriodicListener.class,listener);
-        if ( event_listeners.getListenerCount() == 0 )
+        if ( event_listeners.size() == 0 )
            unsubscribe_event(event_identifier);
 
     }
@@ -101,14 +103,10 @@ public class TangoPeriodic extends EventDispatcher implements java.io.Serializab
 	//==============================================================
     private void fireTangoPeriodicEvent(TangoPeriodic tangoPeriodic, EventData eventData) {
         TangoPeriodicEvent tangoPeriodicEvent = new TangoPeriodicEvent(tangoPeriodic, eventData);
-        // Guaranteed to return a non null array
-        Object [] listeners = event_listeners.getListenerList();
-        // Process the listeners last to first, notifying
-        // those that are interested in this event
-        for (int i = listeners.length-2 ; i>=0 ; i-=2 ) {
-            if (listeners[i] == ITangoPeriodicListener.class) {
-                ((ITangoPeriodicListener)listeners[i+1]).periodic(tangoPeriodicEvent);
-            }
+        // Notifying those that are interested in this event
+        ArrayList<EventListener> listeners = event_listeners.getListeners(ITangoPeriodicListener.class);
+        for (EventListener eventListener : listeners) {
+            ((ITangoPeriodicListener) eventListener).periodic(tangoPeriodicEvent);
         }
     }
 	//==============================================================
