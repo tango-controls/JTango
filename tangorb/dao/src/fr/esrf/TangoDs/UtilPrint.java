769dbb92f1081356cecfdef3cfd6bbba7988d250
Pascal Verdier <verdier@esrf.fr>
2013-07-31 08:02:46 +0000

    Remove double licencing headers.

--- tangorb/dao/src/fr/esrf/TangoDs/UtilPrint.java
+++ tangorb/dao/src/fr/esrf/TangoDs/UtilPrint.java
@@ -36,35 +36,6 @@ package fr.esrf.TangoDs;
 
 import org.apache.log4j.*;
 
-/**
- * This class is a TANGO core class used to manage the different level of
- * printing in a TANGO device server. Within a device server, there is one
- * object of  this class for each printing level (actually four)
- *
- * @author	$Author$
-//
-// Copyright (C) :      2004,2005,2006,2007,2008,2009
-//						European Synchrotron Radiation Facility
-//                      BP 220, Grenoble 38043
-//                      FRANCE
-//
-// This file is part of Tango.
-//
-// Tango is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// Tango is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public License
-// along with Tango.  If not, see <http://www.gnu.org/licenses/>.
-//
- * @version	$Revision$
- */
  
 public class UtilPrint
 {
