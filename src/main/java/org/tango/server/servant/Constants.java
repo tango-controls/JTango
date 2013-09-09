85cb7002b0c246e0c2ab900dc32d2a67c1ade739
Gwenaelle Abeille <gwenaelle.abeille@synchrotron-soleil.fr>
2013-09-09 08:13:03 +0000

    change attribute property name for memorized value

--- src/main/java/org/tango/server/servant/Constants.java
+++ src/main/java/org/tango/server/servant/Constants.java
@@ -1,26 +1,26 @@
 /**
- * Copyright (C) :     2012
- *
- * 	Synchrotron Soleil
- * 	L'Orme des merisiers
- * 	Saint Aubin
- * 	BP48
- * 	91192 GIF-SUR-YVETTE CEDEX
- *
+ * Copyright (C) : 2012
+ * 
+ * Synchrotron Soleil
+ * L'Orme des merisiers
+ * Saint Aubin
+ * BP48
+ * 91192 GIF-SUR-YVETTE CEDEX
+ * 
  * This file is part of Tango.
- *
+ * 
  * Tango is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * Tango is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with Tango.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Tango. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.tango.server.servant;
 
@@ -35,7 +35,7 @@ public final class Constants {
      */
     public static final String ADMIN_DEVICE_DOMAIN = "dserver";
 
-    public static final String MEMORIZED_VALUE = "memorizedValue";
+    public static final String MEMORIZED_VALUE = "__Value";
     public static final String MEMORIZED_VALUE_DIM = "memorizedValueDim";
 
     public static final String IS_POLLED = "isPolled";
