ecef0b7b6ea01424a12b82bfb15a41909129b2e4
abeilleg <abeilleg@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2013-09-30 13:28:47 +0000

    add  devencoded change event check

--- server/src/main/java/org/tango/server/attribute/AttributeImpl.java
+++ server/src/main/java/org/tango/server/attribute/AttributeImpl.java
@@ -1,26 +1,26 @@
 /**
- * Copyright (C) :     2012
- *
- * 	Synchrotron Soleil
- * 	L'Orme des merisiers
- * 	Saint Aubin
- * 	BP48
- * 	91192 GIF-SUR-YVETTE CEDEX
- *
+ * Copyright (C) : 2012
+ * 
+ * Synchrotron Soleil
+ * L'Orme des merisiers
+ * Saint Aubin
+ * BP48
+ * 91192 GIF-SUR-YVETTE CEDEX
+ * 
  * This file is part of Tango.
- *
+ * 
  * Tango is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * Tango is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with Tango.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Tango. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.tango.server.attribute;
 
@@ -52,6 +52,7 @@ import org.tango.utils.DevFailedUtils;
 import fr.esrf.Tango.AttrDataFormat;
 import fr.esrf.Tango.AttrQuality;
 import fr.esrf.Tango.AttrWriteType;
+import fr.esrf.Tango.DevEncoded;
 import fr.esrf.Tango.DevError;
 import fr.esrf.Tango.DevFailed;
 import fr.esrf.Tango.DevState;
@@ -729,6 +730,10 @@ public final class AttributeImpl extends DeviceBehaviorObject implements Compara
         result = result || byte.class.isAssignableFrom(config.getScalarType());
         result = result || short.class.isAssignableFrom(config.getScalarType());
         return result;
+    }
+
+    public boolean isDevEncoded() {
+        return DevEncoded.class.isAssignableFrom(config.getScalarType());
     }
 
     public boolean isBoolean() {
