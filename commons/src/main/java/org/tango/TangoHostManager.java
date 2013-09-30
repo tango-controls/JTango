500ba322c5ef036f06a3b3606ecf1f056e41eadf
abeilleg <abeilleg@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2013-09-30 08:17:35 +0000

    add getFirstFullTangoHost

--- commons/src/main/java/org/tango/TangoHostManager.java
+++ commons/src/main/java/org/tango/TangoHostManager.java
@@ -1,5 +1,7 @@
 package org.tango;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
@@ -38,61 +40,86 @@ public final class TangoHostManager {
     }
 
     public static String getTangoHost() {
-	return TANGO_HOST;
+        return TANGO_HOST;
     }
 
     private static void checkTangoHost() throws DevFailed {
-	if (!isChecked) {
-	    LOGGER.debug("TANGO_HOST is {} ", TANGO_HOST);
-	    if (TANGO_HOST_PORT_MAP.isEmpty()) {
-		if (TANGO_HOST == null) {
-		    DevFailedUtils.throwDevFailed("CONFIG_ERROR", "tango host not defined");
-		}
-		if (TANGO_HOST.indexOf(':') < 0) {
-		    DevFailedUtils
-			    .throwDevFailed("CONFIG_ERROR", "tango host not defined correctly. Must be host:port");
-		}
-		// Check if there is more than one Tango Host
-		StringTokenizer stk;
-		if (TANGO_HOST.indexOf(',') > 0) {
-		    stk = new StringTokenizer(TANGO_HOST, ",");
-		} else {
-		    stk = new StringTokenizer(TANGO_HOST);
-		}
+        if (!isChecked) {
+            LOGGER.debug("TANGO_HOST is {} ", TANGO_HOST);
+            if (TANGO_HOST_PORT_MAP.isEmpty()) {
+                if (TANGO_HOST == null) {
+                    DevFailedUtils.throwDevFailed("CONFIG_ERROR", "tango host not defined");
+                }
+                if (TANGO_HOST.indexOf(':') < 0) {
+                    DevFailedUtils
+                            .throwDevFailed("CONFIG_ERROR", "tango host not defined correctly. Must be host:port");
+                }
+                // Check if there is more than one Tango Host
+                StringTokenizer stk;
+                if (TANGO_HOST.indexOf(',') > 0) {
+                    stk = new StringTokenizer(TANGO_HOST, ",");
+                } else {
+                    stk = new StringTokenizer(TANGO_HOST);
+                }
 
-		while (stk.hasMoreTokens()) {
-		    // Get each Tango_host
-		    final String th = stk.nextToken();
-		    final StringTokenizer stk2 = new StringTokenizer(th, ":");
-		    final String host = stk2.nextToken();
-		    final String port = stk2.nextToken();
-		    // System.out.println(tangoHostPortMap);
-		    TANGO_HOST_PORT_MAP.put(host, port);
-		}
-	    }
-	    isChecked = true;
-	}
+                while (stk.hasMoreTokens()) {
+                    // Get each Tango_host
+                    final String th = stk.nextToken();
+                    final StringTokenizer stk2 = new StringTokenizer(th, ":");
+                    final String host = stk2.nextToken();
+                    final String port = stk2.nextToken();
+                    // System.out.println(tangoHostPortMap);
+                    TANGO_HOST_PORT_MAP.put(host, port);
+                }
+            }
+            isChecked = true;
+        }
     }
 
     public static String getFirstTangoHost() throws DevFailed {
-	checkTangoHost();
-	final Map.Entry<String, String> entrySet = TANGO_HOST_PORT_MAP.entrySet().iterator().next();
-	return entrySet.getKey() + ":" + entrySet.getValue();
+        checkTangoHost();
+        final Map.Entry<String, String> entrySet = TANGO_HOST_PORT_MAP.entrySet().iterator().next();
+        return entrySet.getKey() + ":" + entrySet.getValue();
     }
 
     public static String getFirstHost() throws DevFailed {
-	checkTangoHost();
-	final Map.Entry<String, String> entrySet = TANGO_HOST_PORT_MAP.entrySet().iterator().next();
-	return entrySet.getKey();
+        checkTangoHost();
+        final Map.Entry<String, String> entrySet = TANGO_HOST_PORT_MAP.entrySet().iterator().next();
+        return entrySet.getKey();
     }
 
     public static String getFirstPort() throws DevFailed {
-	checkTangoHost();
-	final Map.Entry<String, String> entrySet = TANGO_HOST_PORT_MAP.entrySet().iterator().next();
-	return entrySet.getValue();
+        checkTangoHost();
+        final Map.Entry<String, String> entrySet = TANGO_HOST_PORT_MAP.entrySet().iterator().next();
+        return entrySet.getValue();
+    }
+
+    /**
+     * Returns the TANGO_HOST with full qualified name.
+     * 
+     * @param tangoHost default Tango_host
+     * @return the TANGO_HOST with full qualified name.
+     * @throws DevFailed if TANGO_HOST value has a bad syntax.
+     */
+    public static String getFirstFullTangoHost() throws DevFailed {
+        // TODO
+        final String TANGO_HOST_ERROR = "API_GetTangoHostFailed";
+
+        // Get the tango_host
+        String host = getFirstHost();
+
+        try {
+            // Get FQDN for host
+            final InetAddress iadd = InetAddress.getByName(host);
+            host = iadd.getCanonicalHostName();
+        } catch (final UnknownHostException e) {
+            DevFailedUtils.throwDevFailed(TANGO_HOST_ERROR, e.toString());
+        }
+
+        return host + ':' + getFirstPort();
     }
 
     public static Map<String, String> getTangoHostPortMap() {
-	return new HashMap<String, String>(TANGO_HOST_PORT_MAP);
+        return new HashMap<String, String>(TANGO_HOST_PORT_MAP);
     }
 }
