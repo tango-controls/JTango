b34274374f09a04346b0918d33303050da1681e6
Gwenaelle Abeille <gwenaelle.abeille@synchrotron-soleil.fr>
2013-10-14 13:52:40 +0000

    DataReady event: The AttDataReady object is build in user's device.

--- server/src/main/java/org/tango/server/events/EventImpl.java
+++ server/src/main/java/org/tango/server/events/EventImpl.java
@@ -1,26 +1,26 @@
 /**
- * Copyright (C) :     2012
- *
- * 	Synchrotron Soleil
- * 	L'Orme des merisiers
- * 	Saint Aubin
- * 	BP48
- * 	91192 GIF-SUR-YVETTE CEDEX
- *
+ * Copyright (C) : 2012
+ * 
+ * Synchrotron Soleil
+ * L'Orme des merisiers
+ * Saint Aubin
+ * BP48
+ * 91192 GIF-SUR-YVETTE CEDEX
+ * 
  * This file is part of Tango.
- *
+ * 
  * Tango is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * Tango is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with Tango.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Tango. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.tango.server.events;
 
@@ -107,7 +107,6 @@ final class EventImpl {
                 sendEvent(eventSocket, fullName, attributeValue);
             }
         }
-
         xlogger.exit();
     }
 
@@ -131,17 +130,13 @@ final class EventImpl {
     /**
      * Send a data ready event
      * 
-     * @param eventSocket
-     * @param fullName
-     * @param dataReady
+     * @param eventSocket the socket to send event
+     * @param fullName event full name
+     * @param dataReady AttrDataReady object
      * @throws DevFailed
      */
-    void pushDataReadyEvent(final ZMQ.Socket eventSocket, final String fullName, int counter) throws DevFailed {
+    void pushEvent(final ZMQ.Socket eventSocket, final String fullName, final AttDataReady dataReady) throws DevFailed {
         xlogger.entry();
-        final AttDataReady dataReady = new AttDataReady();
-        dataReady.name = attribute.getName();
-        dataReady.data_type = attribute.getTangoType();
-        dataReady.ctr = counter;
         try {
             eventSocket.sendMore(fullName);
             eventSocket.send(EventConstants.LITTLE_ENDIAN, ZMQ.SNDMORE);
