e954310e340bf131a74fe64af66fd7a006114352
abeilleg <abeilleg@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2013-10-28 15:18:29 +0000

    avoid NPE when there is no data

--- server/src/main/java/org/tango/server/idl/CleverAttrValUnion.java
+++ server/src/main/java/org/tango/server/idl/CleverAttrValUnion.java
@@ -52,39 +52,39 @@ public final class CleverAttrValUnion {
     private static final Map<AttributeDataType, String> METHOD_MAP = new HashMap<AttributeDataType, String>();
 
     static {
-	METHOD_MAP.put(AttributeDataType.ATT_BOOL, "bool_att_value");
-	METHOD_MAP.put(AttributeDataType.ATT_SHORT, "short_att_value");
-	METHOD_MAP.put(AttributeDataType.ATT_USHORT, "ushort_att_value");
-	METHOD_MAP.put(AttributeDataType.ATT_LONG, "long_att_value");
-	METHOD_MAP.put(AttributeDataType.ATT_ULONG, "ulong_att_value");
-	METHOD_MAP.put(AttributeDataType.ATT_LONG64, "long64_att_value");
-	METHOD_MAP.put(AttributeDataType.ATT_ULONG64, "ulong64_att_value");
-	METHOD_MAP.put(AttributeDataType.ATT_FLOAT, "float_att_value");
-	METHOD_MAP.put(AttributeDataType.ATT_DOUBLE, "double_att_value");
-	METHOD_MAP.put(AttributeDataType.ATT_STRING, "string_att_value");
-	METHOD_MAP.put(AttributeDataType.ATT_STATE, "state_att_value");
-	METHOD_MAP.put(AttributeDataType.DEVICE_STATE, "dev_state_att");
-	METHOD_MAP.put(AttributeDataType.ATT_UCHAR, "uchar_att_value");
-	METHOD_MAP.put(AttributeDataType.ATT_ENCODED, "encoded_att_value");
+        METHOD_MAP.put(AttributeDataType.ATT_BOOL, "bool_att_value");
+        METHOD_MAP.put(AttributeDataType.ATT_SHORT, "short_att_value");
+        METHOD_MAP.put(AttributeDataType.ATT_USHORT, "ushort_att_value");
+        METHOD_MAP.put(AttributeDataType.ATT_LONG, "long_att_value");
+        METHOD_MAP.put(AttributeDataType.ATT_ULONG, "ulong_att_value");
+        METHOD_MAP.put(AttributeDataType.ATT_LONG64, "long64_att_value");
+        METHOD_MAP.put(AttributeDataType.ATT_ULONG64, "ulong64_att_value");
+        METHOD_MAP.put(AttributeDataType.ATT_FLOAT, "float_att_value");
+        METHOD_MAP.put(AttributeDataType.ATT_DOUBLE, "double_att_value");
+        METHOD_MAP.put(AttributeDataType.ATT_STRING, "string_att_value");
+        METHOD_MAP.put(AttributeDataType.ATT_STATE, "state_att_value");
+        METHOD_MAP.put(AttributeDataType.DEVICE_STATE, "dev_state_att");
+        METHOD_MAP.put(AttributeDataType.ATT_UCHAR, "uchar_att_value");
+        METHOD_MAP.put(AttributeDataType.ATT_ENCODED, "encoded_att_value");
     }
 
     private static final Map<AttributeDataType, Class<?>> PARAM_MAP = new HashMap<AttributeDataType, Class<?>>();
 
     static {
-	PARAM_MAP.put(AttributeDataType.ATT_BOOL, boolean[].class);
-	PARAM_MAP.put(AttributeDataType.ATT_SHORT, short[].class);
-	PARAM_MAP.put(AttributeDataType.ATT_USHORT, short[].class);
-	PARAM_MAP.put(AttributeDataType.ATT_LONG, int[].class);
-	PARAM_MAP.put(AttributeDataType.ATT_ULONG, int[].class);
-	PARAM_MAP.put(AttributeDataType.ATT_LONG64, long[].class);
-	PARAM_MAP.put(AttributeDataType.ATT_ULONG64, long[].class);
-	PARAM_MAP.put(AttributeDataType.ATT_FLOAT, float[].class);
-	PARAM_MAP.put(AttributeDataType.ATT_DOUBLE, double[].class);
-	PARAM_MAP.put(AttributeDataType.ATT_STRING, String[].class);
-	PARAM_MAP.put(AttributeDataType.ATT_STATE, DevState[].class);
-	PARAM_MAP.put(AttributeDataType.DEVICE_STATE, DevState.class);
-	PARAM_MAP.put(AttributeDataType.ATT_UCHAR, byte[].class);
-	PARAM_MAP.put(AttributeDataType.ATT_ENCODED, DevEncoded[].class);
+        PARAM_MAP.put(AttributeDataType.ATT_BOOL, boolean[].class);
+        PARAM_MAP.put(AttributeDataType.ATT_SHORT, short[].class);
+        PARAM_MAP.put(AttributeDataType.ATT_USHORT, short[].class);
+        PARAM_MAP.put(AttributeDataType.ATT_LONG, int[].class);
+        PARAM_MAP.put(AttributeDataType.ATT_ULONG, int[].class);
+        PARAM_MAP.put(AttributeDataType.ATT_LONG64, long[].class);
+        PARAM_MAP.put(AttributeDataType.ATT_ULONG64, long[].class);
+        PARAM_MAP.put(AttributeDataType.ATT_FLOAT, float[].class);
+        PARAM_MAP.put(AttributeDataType.ATT_DOUBLE, double[].class);
+        PARAM_MAP.put(AttributeDataType.ATT_STRING, String[].class);
+        PARAM_MAP.put(AttributeDataType.ATT_STATE, DevState[].class);
+        PARAM_MAP.put(AttributeDataType.DEVICE_STATE, DevState.class);
+        PARAM_MAP.put(AttributeDataType.ATT_UCHAR, byte[].class);
+        PARAM_MAP.put(AttributeDataType.ATT_ENCODED, DevEncoded[].class);
     }
 
     private CleverAttrValUnion() {
@@ -100,29 +100,32 @@ public final class CleverAttrValUnion {
      * @throws DevFailed
      */
     public static Object get(final AttrValUnion union, final AttrDataFormat format) throws DevFailed {
-	Object result = null;
-	if (union != null) {
-	    final AttributeDataType discriminator = union.discriminator();
-	    try {
-		final Method method = union.getClass().getMethod(METHOD_MAP.get(discriminator));
-		result = method.invoke(union);
-	    } catch (final IllegalArgumentException e) {
-		DevFailedUtils.throwDevFailed(e);
-	    } catch (final IllegalAccessException e) {
-		DevFailedUtils.throwDevFailed(e);
-	    } catch (final InvocationTargetException e) {
-		DevFailedUtils.throwDevFailed(e.getCause());
-	    } catch (final SecurityException e) {
-		DevFailedUtils.throwDevFailed(e);
-	    } catch (final NoSuchMethodException e) {
-		DevFailedUtils.throwDevFailed(e);
-	    }
-	    if (format.equals(AttrDataFormat.SCALAR) && !discriminator.equals(AttributeDataType.DEVICE_STATE)) {
-		// for scalar except state, get only first value
-		result = Array.get(result, 0);
-	    }
-	}
-	return result;
+        Object result = null;
+        if (union != null) {
+            final AttributeDataType discriminator = union.discriminator();
+            if (discriminator.value() == AttributeDataType._NO_DATA) {
+                throw DevFailedUtils.newDevFailed("there is not data");
+            }
+            try {
+                final Method method = union.getClass().getMethod(METHOD_MAP.get(discriminator));
+                result = method.invoke(union);
+            } catch (final IllegalArgumentException e) {
+                DevFailedUtils.throwDevFailed(e);
+            } catch (final IllegalAccessException e) {
+                DevFailedUtils.throwDevFailed(e);
+            } catch (final InvocationTargetException e) {
+                DevFailedUtils.throwDevFailed(e.getCause());
+            } catch (final SecurityException e) {
+                DevFailedUtils.throwDevFailed(e);
+            } catch (final NoSuchMethodException e) {
+                DevFailedUtils.throwDevFailed(e);
+            }
+            if (format.equals(AttrDataFormat.SCALAR) && !discriminator.equals(AttributeDataType.DEVICE_STATE)) {
+                // for scalar except state, get only first value
+                result = Array.get(result, 0);
+            }
+        }
+        return result;
     }
 
     /**
@@ -134,38 +137,38 @@ public final class CleverAttrValUnion {
      * @throws DevFailed
      */
     public static AttrValUnion set(final int tangoType, final Object value) throws DevFailed {
-	final AttributeDataType discriminator = AttributeTangoType.getTypeFromTango(tangoType).getAttributeDataType();
-	final AttrValUnion union = new AttrValUnion();
-	Object array = null;
-	if (value.getClass().isArray()) {
-	    // convert to array of primitives if necessary
-	    array = org.tango.utils.ArrayUtils.toPrimitiveArray(value);
-	} else {
-	    // put in an array before inserting for scalars
-	    array = Array.newInstance(AttributeTangoType.getTypeFromTango(tangoType).getType(), 1);
-	    try {
-		Array.set(array, 0, value);
-	    } catch (final IllegalArgumentException e) {
-		DevFailedUtils.throwDevFailed(ExceptionMessages.ATTR_OPT_PROP, value.getClass().getCanonicalName()
-			+ " is not of the good type");
-	    }
-	}
-	try {
-	    final Method method = union.getClass().getMethod(METHOD_MAP.get(discriminator),
-		    PARAM_MAP.get(discriminator));
-	    method.invoke(union, array);
-	} catch (final IllegalArgumentException e) {
-	    DevFailedUtils.throwDevFailed(ExceptionMessages.ATTR_OPT_PROP, value.getClass().getCanonicalName()
-		    + " is not of the good type");
-	} catch (final IllegalAccessException e) {
-	    DevFailedUtils.throwDevFailed(e);
-	} catch (final InvocationTargetException e) {
-	    DevFailedUtils.throwDevFailed(e.getCause());
-	} catch (final SecurityException e) {
-	    DevFailedUtils.throwDevFailed(e);
-	} catch (final NoSuchMethodException e) {
-	    DevFailedUtils.throwDevFailed(e);
-	}
-	return union;
+        final AttributeDataType discriminator = AttributeTangoType.getTypeFromTango(tangoType).getAttributeDataType();
+        final AttrValUnion union = new AttrValUnion();
+        Object array = null;
+        if (value.getClass().isArray()) {
+            // convert to array of primitives if necessary
+            array = org.tango.utils.ArrayUtils.toPrimitiveArray(value);
+        } else {
+            // put in an array before inserting for scalars
+            array = Array.newInstance(AttributeTangoType.getTypeFromTango(tangoType).getType(), 1);
+            try {
+                Array.set(array, 0, value);
+            } catch (final IllegalArgumentException e) {
+                DevFailedUtils.throwDevFailed(ExceptionMessages.ATTR_OPT_PROP, value.getClass().getCanonicalName()
+                        + " is not of the good type");
+            }
+        }
+        try {
+            final Method method = union.getClass().getMethod(METHOD_MAP.get(discriminator),
+                    PARAM_MAP.get(discriminator));
+            method.invoke(union, array);
+        } catch (final IllegalArgumentException e) {
+            DevFailedUtils.throwDevFailed(ExceptionMessages.ATTR_OPT_PROP, value.getClass().getCanonicalName()
+                    + " is not of the good type");
+        } catch (final IllegalAccessException e) {
+            DevFailedUtils.throwDevFailed(e);
+        } catch (final InvocationTargetException e) {
+            DevFailedUtils.throwDevFailed(e.getCause());
+        } catch (final SecurityException e) {
+            DevFailedUtils.throwDevFailed(e);
+        } catch (final NoSuchMethodException e) {
+            DevFailedUtils.throwDevFailed(e);
+        }
+        return union;
     }
 }
