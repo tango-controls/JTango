47f4a148e34590a01ebe95f1f49f4a711500d711
abeilleg <abeilleg@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2014-02-19 10:06:56 +0000

    prepare release

---
+++ server/src/test/java/org/tango/server/testserver/Kill.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (C) :     2012
+ *
+ * 	Synchrotron Soleil
+ * 	L'Orme des merisiers
+ * 	Saint Aubin
+ * 	BP48
+ * 	91192 GIF-SUR-YVETTE CEDEX
+ *
+ * This file is part of Tango.
+ *
+ * Tango is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Tango is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Tango.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.tango.server.testserver;
+
+public class Kill {
+
+    /**
+     * @param args
+     * @throws DevFailed
+     */
+//    public static void main(final String[] args) throws DevFailed {
+//        EventServer.start();
+//        try {
+//            Thread.sleep(1000);
+//        } catch (final InterruptedException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//        ServerManager.getInstance().stop();
+//        try {
+//            Thread.sleep(2000);
+//        } catch (final InterruptedException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//        EventServer.start();
+//        try {
+//            Thread.sleep(1000);
+//        } catch (final InterruptedException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//        ServerManager.getInstance().stop();
+//        System.out.println("stop device out");
+//        // System.exit(-1);
+//
+//    }
+}
