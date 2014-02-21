1a8f06fed8f5dc9eb2fa741e5de3e6c93964cc1a
Pascal Verdier <verdier@esrf.fr>
2014-02-21 14:46:21 +0000

    Remove compiler warnings

--- common/src/fr/esrf/TangoDs/AttrManip.java
+++ common/src/fr/esrf/TangoDs/AttrManip.java
@@ -39,180 +39,99 @@ import java.util.StringTokenizer;
 
 
 /**
- * This class is a TANGO core class used to print attribute value follwing
+ * This class is a TANGO core class used to print attribute value following
  * the format defined as one of the attribute property
- *
- * @author	$Author$
-//
-// Copyright (C) :      2004,2005,2006,2007,2008,2009
-//						European Synchrotron Radiation Facility
-//                      BP 220, Grenoble 38043
-//                      FRANCE
-//
-// This file is part of Tango.
-//
-// Tango is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// Tango is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public License
-// along with Tango.  If not, see <http://www.gnu.org/licenses/>.
-//
-// $Revision$
-//
-// $Log$
-// Revision 1.7  2009/01/16 12:52:13  pascal_verdier
-// IntelliJIdea warnings removed.
-//
-// Revision 1.6  2008/10/10 11:35:27  pascal_verdier
-// Headers changed for LGPL conformity.
-//
-// Revision 1.5  2007/09/13 09:22:32  ounsy
-// Add java.io.serializable to the dtata classe
-//
-// Revision 1.4  2007/08/23 08:32:59  ounsy
-// updated change from api/java
-//
-// Revision 3.6  2005/12/02 09:55:02  pascal_verdier
-// java import have been optimized.
-//
-// Revision 3.5  2004/03/12 14:07:56  pascal_verdier
-// Use JacORB-2.1
-//
-// Revision 2.0  2003/01/09 16:02:57  taurel
-// - Update release number before using SourceForge
-//
-// Revision 1.1.1.1  2003/01/09 15:54:39  taurel
-// Imported sources into CVS before using SourceForge
-//
-// Revision 1.6  2001/10/10 08:11:22  taurel
-// See Tango WEB pages for list of changes
-//
-// Revision 1.5  2001/07/04 15:06:35  taurel
-// Many changes due to new release
-//
-//
-// copyleft :           European Synchrotron Radiation Facility
-//                      BP 220, Grenoble 38043
-//                      FRANCE
-//
-//-============================================================================
-
-package fr.esrf.TangoDs;
-
-import java.text.DecimalFormat;
-import java.util.StringTokenizer;
-
-
-/**
- * This class is a TANGO core class used to print attribute value follwing
- * the format defined as one of the attribute property
- *
- * @author	$Author$
- * @version	$Revision$
+ * @author $Author$
+ * @version $Revision$
  */
- 
-public class AttrManip implements java.io.Serializable
-{
-	static DecimalFormat df = null;
-	static boolean pos = false;
-	static int prec = 0;
-	static boolean point = false;
 
-/**
- * Format the attribute value (value is an int).
- *
- * This method returns the input value as a string formatted according to the
- * format parameter. The syntax of this format string is the syntax defined
- * for the attribute format property.
- *
- * @param format The format string (as defined in the attribute property format)
- * @param value The attribute value
- * @return The input value as a string
- */
-  	
-	public static String format(String format,int value)
-	{
-		prec = 0;
-		point = false;
-		df = null;
+public class AttrManip implements java.io.Serializable {
+    static DecimalFormat df = null;
+    static boolean pos = false;
+    static int prec = 0;
+    static boolean point = false;
 
-		analyse_format(format);
-					
-		StringBuffer out = new StringBuffer();		
+    /**
+     * Format the attribute value (value is an int).
+     * <p/>
+     * This method returns the input value as a string formatted according to the
+     * format parameter. The syntax of this format string is the syntax defined
+     * for the attribute format property.
+     *
+     * @param format The format string (as defined in the attribute property format)
+     * @param value  The attribute value
+     * @return The input value as a string
+     */
 
-		if (df != null)						
-			out.append(df.format(value));
-		else
-			out.append(value);
-		if (pos == true)
-		{
-			if (value > 0)
-				out.insert(0,"+");
-			pos = false;
-		}
-		return out.toString();			
-	}
+    public static String format(String format, int value) {
+        prec = 0;
+        point = false;
+        df = null;
 
-	
-/**
- * Format the attribute value (value is a double).
- *
- * This method returns the input value as a string formatted according to the
- * format parameter. The syntax of this format string is the syntax defined
- * for the attribute format property.
- *
- * @param format The format string (as defined in the attribute property format)
- * @param value The attribute value
- * @return The input value as a string
- */
- 			
-	public static String format(String format,double value)
-	{
-		prec = 0;
-		point = false;
-		df = null;
-		
-		analyse_format(format);
-		
-		StringBuffer out = new StringBuffer();		
+        analyse_format(format);
 
-		if (df != null)						
-			out.append(df.format(value));
-		else
-			out.append(value);
-			
-		if (pos == true)
-		{
-			if (value > 0)
-				out.insert(0,"+");
-			pos = false;
-		}
-		return out.toString();
-	}
+        StringBuilder out = new StringBuilder();
 
-/**
- * Format the attribute value (value is a string).
- *
- * This method does nothing and only returns the input string. It has been
- * written for compatibility with the other attribuute data type.
- *
- * @param format The format string (not used in this case)
- * @param value The attribute value
- * @return The input value
- */
- 	
-	@SuppressWarnings({"UNUSED_SYMBOL"})
-	public static String format(String format,String value)
-	{
-		return value;
-	}
+        if (df != null)
+            out.append(df.format(value));
+        else
+            out.append(value);
+        if (pos) {
+            if (value > 0)
+                out.insert(0, "+");
+            pos = false;
+        }
+        return out.toString();
+    }
+
+
+    /**
+     * Format the attribute value (value is a double).
+     * <p/>
+     * This method returns the input value as a string formatted according to the
+     * format parameter. The syntax of this format string is the syntax defined
+     * for the attribute format property.
+     *
+     * @param format The format string (as defined in the attribute property format)
+     * @param value  The attribute value
+     * @return The input value as a string
+     */
+
+    public static String format(String format, double value) {
+        prec = 0;
+        point = false;
+        df = null;
+
+        analyse_format(format);
+
+        StringBuilder out = new StringBuilder();
+
+        if (df != null)
+            out.append(df.format(value));
+        else
+            out.append(value);
+
+        if (pos) {
+            if (value > 0)
+                out.insert(0, "+");
+            pos = false;
+        }
+        return out.toString();
+    }
+
+    /**
+     * Format the attribute value (value is a string).
+     * <p/>
+     * This method does nothing and only returns the input string. It has been
+     * written for compatibility with the other attribute data type.
+     *
+     * @param format The format string (not used in this case)
+     * @param value  The attribute value
+     * @return The input value
+     */
+    public static String format(String format, String value) {
+        return value;
+    }
 
 //+-------------------------------------------------------------------------
 //
@@ -226,22 +145,20 @@ public class AttrManip implements java.io.Serializable
 //
 //--------------------------------------------------------------------------
 
-	private static void analyse_format(String format)
-	{
-		StringTokenizer st = new StringTokenizer(format.toLowerCase(),";\n");
+    private static void analyse_format(String format) {
+        StringTokenizer st = new StringTokenizer(format.toLowerCase(), ";\n");
 
-		int token_nb = st.countTokens();
-		String[] m_array = new String[token_nb];
-		int i = 0;
-                while (st.hasMoreTokens())
-                {
-                        m_array[i] = st.nextToken();
-			i++;
-                }
+        int token_nb = st.countTokens();
+        String[] m_array = new String[token_nb];
+        int i = 0;
+        while (st.hasMoreTokens()) {
+            m_array[i] = st.nextToken();
+            i++;
+        }
 
-		for (i = 0;i < token_nb;i++)
-			execute_manip(m_array[i]);
-	}
+        for (i = 0; i < token_nb; i++)
+            execute_manip(m_array[i]);
+    }
 
 //+-------------------------------------------------------------------------
 //
@@ -260,65 +177,50 @@ public class AttrManip implements java.io.Serializable
 // argument : in : 	- s : The C++ like manipulator
 //
 //--------------------------------------------------------------------------
-		
-	private static void execute_manip(String s)
-	{
-		if (s.equals("showpos") == true)
-			pos = true;
-		else if (s.equals("fixed") == true)
-		{
-			if (df == null)
-			{
-				df = new DecimalFormat("0.######");
-				if (prec != 0)
-					df.setMaximumFractionDigits(prec);
-				if (point != false)
-				{
-					if (prec != 0)
-						df.setMinimumFractionDigits(prec);
-					else
-						df.setMinimumFractionDigits(6);
-				}
-			}
-		}
-		else if (s.equals("scientific") == true)
-		{
-			if (df == null)
-			{
-				df = new DecimalFormat("0.######E00");
-				if (prec != 0)
-					df.setMaximumFractionDigits(prec);
-				if (point != false)
-				{
-					if (prec != 0)
-						df.setMinimumFractionDigits(prec);
-					else
-						df.setMinimumFractionDigits(6);
-				}
-			}
-		}
-		else if (s.length() >= 15)
-		{
-			if (s.substring(0,13).equals("setprecision(") == true)
-			{
-				String prec_str = s.substring(13,s.length() - 1);
-				int tmp_prec = Integer.parseInt(prec_str);
-				if (df != null)
-					df.setMaximumFractionDigits(tmp_prec);
-				prec = tmp_prec;
-			}
-		}
-		else if (s.equals("showpoint") == true)
-		{
-			if (df != null)
-			{
-				if (prec != 0)
-					df.setMinimumFractionDigits(prec);
-				else
-					df.setMinimumFractionDigits(6);
-			}
-			else
-				point = true;
-		}
-	}
+
+    private static void execute_manip(String s) {
+        if (s.equals("showpos"))
+            pos = true;
+        else if (s.equals("fixed")) {
+            if (df == null) {
+                df = new DecimalFormat("0.######");
+                if (prec != 0)
+                    df.setMaximumFractionDigits(prec);
+                if (!point) {
+                    if (prec != 0)
+                        df.setMinimumFractionDigits(prec);
+                    else
+                        df.setMinimumFractionDigits(6);
+                }
+            }
+        } else if (s.equals("scientific")) {
+            if (df == null) {
+                df = new DecimalFormat("0.######E00");
+                if (prec != 0)
+                    df.setMaximumFractionDigits(prec);
+                if (!point) {
+                    if (prec != 0)
+                        df.setMinimumFractionDigits(prec);
+                    else
+                        df.setMinimumFractionDigits(6);
+                }
+            }
+        } else if (s.length() >= 15) {
+            if (s.substring(0, 13).equals("setprecision(")) {
+                String prec_str = s.substring(13, s.length() - 1);
+                int tmp_prec = Integer.parseInt(prec_str);
+                if (df != null)
+                    df.setMaximumFractionDigits(tmp_prec);
+                prec = tmp_prec;
+            }
+        } else if (s.equals("showpoint")) {
+            if (df != null) {
+                if (prec != 0)
+                    df.setMinimumFractionDigits(prec);
+                else
+                    df.setMinimumFractionDigits(6);
+            } else
+                point = true;
+        }
+    }
 }
