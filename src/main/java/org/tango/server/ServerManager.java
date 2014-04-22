7f59a4f02489bfb2a084e668bddbb7d22641f5ea
Gwenaelle Abeille <gwenaelle.abeille@synchrotron-soleil.fr>
2014-04-22 09:38:57 +0000

    javadoc

--- src/main/java/org/tango/server/ServerManager.java
+++ src/main/java/org/tango/server/ServerManager.java
@@ -1,26 +1,26 @@
 /**
- * Copyright (C) :     2012
- *
- * 	Synchrotron Soleil
- * 	L'Orme des merisiers
- * 	Saint Aubin
- * 	BP48
- * 	91192 GIF-SUR-YVETTE CEDEX
- *
+ * Copyright (C) : 2012
+ * 
+ * Synchrotron Soleil
+ * L'Orme des merisiers
+ * Saint Aubin
+ * BP48
+ * 91192 GIF-SUR-YVETTE CEDEX
+ * 
  * This file is part of Tango.
- *
+ * 
  * Tango is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * Tango is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with Tango.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Tango. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.tango.server;
 
@@ -378,6 +378,11 @@ public final class ServerManager {
 
     }
 
+    /**
+     * Set the transaction type for all server. Overrides {@link Device#transactionType()}
+     * 
+     * @param transactionType
+     */
     public void setTransactionType(final TransactionType transactionType) {
         this.transactionType = transactionType;
     }
