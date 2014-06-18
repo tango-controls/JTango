a3d22cccfa977da8b3211a6a71b243ea917ccefb
Pascal Verdier <verdier@esrf.fr>
2014-06-18 08:36:14 +0000

    Add InterfaceChange event feature.

---
+++ common/src/fr/esrf/TangoApi/events/ITangoInterfaceChangeListener.java
@@ -0,0 +1,46 @@
+//+======================================================================
+// $Source$
+//
+// Project:   Tango
+//
+// Description:  java source code for the TANGO client/server API.
+//
+// $Author: pascal_verdier $
+//
+// Copyright (C) :      2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,
+//						European Synchrotron Radiation Facility
+//                      BP 220, Grenoble 38043
+//                      FRANCE
+//
+// This file is part of Tango.
+//
+// Tango is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Tango is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with Tango.  If not, see <http://www.gnu.org/licenses/>.
+//
+// $Revision: 25296 $
+//
+//-======================================================================
+
+
+package fr.esrf.TangoApi.events;
+
+
+import java.util.EventListener;
+
+/**
+ *
+ * @author  pascal_verdier
+ */
+public interface ITangoInterfaceChangeListener extends EventListener {
+    public void interface_change(TangoInterfaceChangeEvent e);
+}
