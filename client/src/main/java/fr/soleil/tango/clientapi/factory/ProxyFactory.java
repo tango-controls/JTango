512a696a1a0c597c7c36c650366e71368e69681b
Gwenaelle Abeille <gwenaelle.abeille@synchrotron-soleil.fr>
2015-04-17 10:00:17 +0000

    TANGOCORE-62: force timeout only if defaut is superior

--- client/src/main/java/fr/soleil/tango/clientapi/factory/ProxyFactory.java
+++ client/src/main/java/fr/soleil/tango/clientapi/factory/ProxyFactory.java
@@ -27,25 +27,25 @@ public final class ProxyFactory {
 
     private static class LRUMap<K, V> extends LinkedHashMap<K, V> {
 
-	private static final long serialVersionUID = -6898381007090501228L;
+        private static final long serialVersionUID = -6898381007090501228L;
 
-	public LRUMap(final int initialCapacity, final float loadFactor, final boolean accessOrder) {
-	    super(initialCapacity, loadFactor, accessOrder);
-	}
+        public LRUMap(final int initialCapacity, final float loadFactor, final boolean accessOrder) {
+            super(initialCapacity, loadFactor, accessOrder);
+        }
 
-	@Override
-	public boolean removeEldestEntry(final Map.Entry<K, V> eldest) {
-	    return size() > MAX_ENTRIES;
-	}
+        @Override
+        public boolean removeEldestEntry(final Map.Entry<K, V> eldest) {
+            return size() > MAX_ENTRIES;
+        }
     }
 
     private ProxyFactory() {
-	// create cache map with a fix capacity. least-recently accessed entries
-	// will be removed
-	devicesMap = new LRUMap<String, DeviceProxy>(MAX_ENTRIES + 1, LOAD, true);
-	attributesMap = new LRUMap<String, AttributeProxy>(MAX_ENTRIES + 1, .75F, true);
-	attributeGroupMap = new LRUMap<String, AttributeGroup>(MAX_ENTRIES + 1, .75F, true);
-	groupMap = new LRUMap<String, Group>(MAX_ENTRIES + 1, .75F, true);
+        // create cache map with a fix capacity. least-recently accessed entries
+        // will be removed
+        devicesMap = new LRUMap<String, DeviceProxy>(MAX_ENTRIES + 1, LOAD, true);
+        attributesMap = new LRUMap<String, AttributeProxy>(MAX_ENTRIES + 1, .75F, true);
+        attributeGroupMap = new LRUMap<String, AttributeGroup>(MAX_ENTRIES + 1, .75F, true);
+        groupMap = new LRUMap<String, Group>(MAX_ENTRIES + 1, .75F, true);
     }
 
     /**
@@ -53,7 +53,7 @@ public final class ProxyFactory {
      * @return instance
      */
     public static ProxyFactory getInstance() {
-	return instance;
+        return instance;
     }
 
     /**
@@ -64,21 +64,23 @@ public final class ProxyFactory {
      * @throws DevFailed
      */
     public DeviceProxy createDeviceProxy(final String deviceName) throws DevFailed {
-	DeviceProxy dev;
-	final String fullDeviceName = TangoUtil.getfullNameForDevice(deviceName);
-	synchronized (devicesMap) {
-	    if (!devicesMap.containsKey(fullDeviceName)) {
-		dev = new DeviceProxy(fullDeviceName);
-		try {
-		    dev.set_timeout_millis(timeout);
-		} catch (final DevFailed e) {
-		}
-		devicesMap.put(fullDeviceName, dev);
-	    } else {
-		dev = devicesMap.get(fullDeviceName);
-	    }
-	}
-	return dev;
+        DeviceProxy dev;
+        final String fullDeviceName = TangoUtil.getfullNameForDevice(deviceName);
+        synchronized (devicesMap) {
+            if (!devicesMap.containsKey(fullDeviceName)) {
+                dev = new DeviceProxy(fullDeviceName);
+                try {
+                    if (dev.get_timeout_millis() < timeout) {
+                        dev.set_timeout_millis(timeout);
+                    }
+                } catch (final DevFailed e) {
+                }
+                devicesMap.put(fullDeviceName, dev);
+            } else {
+                dev = devicesMap.get(fullDeviceName);
+            }
+        }
+        return dev;
     }
 
     /**
@@ -88,18 +90,18 @@ public final class ProxyFactory {
      * @throws DevFailed
      */
     public AttributeProxy createAttributeProxy(final String attrName) throws DevFailed {
-	AttributeProxy attr;
-	final String fullAttrName = TangoUtil.getfullAttributeNameForAttribute(attrName);
-	synchronized (attributesMap) {
-	    if (!attributesMap.containsKey(fullAttrName)) {
-		attr = new AttributeProxy(fullAttrName);
-		attr.set_timeout_millis(timeout);
-		attributesMap.put(fullAttrName, attr);
-	    } else {
-		attr = attributesMap.get(fullAttrName);
-	    }
-	}
-	return attr;
+        AttributeProxy attr;
+        final String fullAttrName = TangoUtil.getfullAttributeNameForAttribute(attrName);
+        synchronized (attributesMap) {
+            if (!attributesMap.containsKey(fullAttrName)) {
+                attr = new AttributeProxy(fullAttrName);
+                attr.set_timeout_millis(timeout);
+                attributesMap.put(fullAttrName, attr);
+            } else {
+                attr = attributesMap.get(fullAttrName);
+            }
+        }
+        return attr;
     }
 
     /**
@@ -110,35 +112,35 @@ public final class ProxyFactory {
      * @throws DevFailed
      */
     public Group createGroup(final String groupName, final String... deviceNames) throws DevFailed {
-	Group grp;
-	final String key = Arrays.toString(deviceNames);
-	synchronized (groupMap) {
-	    if (!groupMap.containsKey(key)) {
-		grp = new Group(groupName);
-		grp.set_timeout_millis(timeout, true);
-		grp.add(deviceNames);
-		groupMap.put(key, grp);
-	    } else {
-		grp = groupMap.get(key);
-	    }
-	}
-	return grp;
+        Group grp;
+        final String key = Arrays.toString(deviceNames);
+        synchronized (groupMap) {
+            if (!groupMap.containsKey(key)) {
+                grp = new Group(groupName);
+                grp.set_timeout_millis(timeout, true);
+                grp.add(deviceNames);
+                groupMap.put(key, grp);
+            } else {
+                grp = groupMap.get(key);
+            }
+        }
+        return grp;
     }
 
     public AttributeGroup createAttributeGroup(final boolean throwExceptions, final String... attributeNames)
-	    throws DevFailed {
-	AttributeGroup grp;
-	final String key = Arrays.toString(attributeNames);
-	synchronized (attributeGroupMap) {
-	    if (!attributeGroupMap.containsKey(key)) {
-		grp = new AttributeGroup(throwExceptions, attributeNames);
-		grp.setTimeout(timeout);
-		attributeGroupMap.put(key, grp);
-	    } else {
-		grp = attributeGroupMap.get(key);
-	    }
-	}
-	return grp;
+            throws DevFailed {
+        AttributeGroup grp;
+        final String key = Arrays.toString(attributeNames);
+        synchronized (attributeGroupMap) {
+            if (!attributeGroupMap.containsKey(key)) {
+                grp = new AttributeGroup(throwExceptions, attributeNames);
+                grp.setTimeout(timeout);
+                attributeGroupMap.put(key, grp);
+            } else {
+                grp = attributeGroupMap.get(key);
+            }
+        }
+        return grp;
     }
 
     /**
@@ -147,32 +149,32 @@ public final class ProxyFactory {
      * @throws DevFailed
      */
     public void setTimout(final int timeout) throws DevFailed {
-	this.timeout = timeout;
-	synchronized (devicesMap) {
-	    for (final DeviceProxy dev : devicesMap.values()) {
-		dev.set_timeout_millis(timeout);
-	    }
-	}
-	synchronized (attributesMap) {
-	    for (final AttributeProxy attr : attributesMap.values()) {
-		attr.getDeviceProxy().set_timeout_millis(timeout);
-		attr.set_timeout_millis(timeout);
-	    }
-	}
-	synchronized (groupMap) {
-	    for (final Group group : groupMap.values()) {
-		group.set_timeout_millis(timeout, true);
-	    }
-	}
-	synchronized (attributeGroupMap) {
-	    for (final AttributeGroup group : attributeGroupMap.values()) {
-		group.setTimeout(timeout);
-	    }
-	}
+        this.timeout = timeout;
+        synchronized (devicesMap) {
+            for (final DeviceProxy dev : devicesMap.values()) {
+                dev.set_timeout_millis(timeout);
+            }
+        }
+        synchronized (attributesMap) {
+            for (final AttributeProxy attr : attributesMap.values()) {
+                attr.getDeviceProxy().set_timeout_millis(timeout);
+                attr.set_timeout_millis(timeout);
+            }
+        }
+        synchronized (groupMap) {
+            for (final Group group : groupMap.values()) {
+                group.set_timeout_millis(timeout, true);
+            }
+        }
+        synchronized (attributeGroupMap) {
+            for (final AttributeGroup group : attributeGroupMap.values()) {
+                group.setTimeout(timeout);
+            }
+        }
     }
 
     public int getTimeout() {
-	return timeout;
+        return timeout;
     }
 
 }
