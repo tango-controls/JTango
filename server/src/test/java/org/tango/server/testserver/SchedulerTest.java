5526d8c8f0373c42819c1ffc1e195b39d051c934
ingvord <tla-ingvord@yandex.ru>
2017-08-01 22:58:28 +0300

    wait 1s for cron job to start

--- server/src/test/java/org/tango/server/testserver/SchedulerTest.java
+++ server/src/test/java/org/tango/server/testserver/SchedulerTest.java
@@ -1,70 +1,79 @@
 /**
  * Copyright (C) :     2012
- *
- * 	Synchrotron Soleil
- * 	L'Orme des merisiers
- * 	Saint Aubin
- * 	BP48
- * 	91192 GIF-SUR-YVETTE CEDEX
- *
+ * <p>
+ * Synchrotron Soleil
+ * L'Orme des merisiers
+ * Saint Aubin
+ * BP48
+ * 91192 GIF-SUR-YVETTE CEDEX
+ * <p>
  * This file is part of Tango.
- *
+ * <p>
  * Tango is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * Tango is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU Lesser General Public License
  * along with Tango.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.tango.server.testserver;
 
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertThat;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import org.junit.Test;
-import org.tango.client.database.DatabaseFactory;
-
 import fr.esrf.Tango.DevFailed;
 import fr.esrf.TangoApi.DeviceProxy;
 import fr.soleil.tango.clientapi.TangoAttribute;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.tango.client.database.DatabaseFactory;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
 
 /**
  * Test schedule
- * 
+ *
  * @author ABEILLE
- * 
+ *
  */
+@RunWith(Parameterized.class)
 public class SchedulerTest extends NoDBDeviceManager {
+
+    @Parameterized.Parameters
+    public static List<Object[]> data() {
+        return Arrays.asList(new Object[100][0]);
+    }
 
     @Test
     public void test() throws DevFailed {
-	final Map<String, String[]> properties = new HashMap<String, String[]>();
-	properties.put("isRunRefresh", new String[] { "true" });
-	DatabaseFactory.getDatabase().setDeviceProperties(JTangoTest.NO_DB_DEVICE_NAME, properties);
-	new DeviceProxy(NoDBDeviceManager.deviceName).command_inout("Init");
-	try {
-	    Thread.sleep(100);
-	} catch (InterruptedException e) {
-	}
-	final TangoAttribute attr = new TangoAttribute(NoDBDeviceManager.deviceName + "/isScheduleRunning");
-	assertThat(attr.read(boolean.class), equalTo(true));
-	properties.put("isRunRefresh", new String[] { "false" });
-	DatabaseFactory.getDatabase().setDeviceProperties(JTangoTest.NO_DB_DEVICE_NAME, properties);
-	new DeviceProxy(NoDBDeviceManager.deviceName).command_inout("Init");
-	try {
-	    Thread.sleep(100);
-	} catch (InterruptedException e) {
-	}
-	assertThat(attr.read(boolean.class), equalTo(false));
+        final Map<String, String[]> properties = new HashMap<String, String[]>();
+        properties.put("isRunRefresh", new String[]{"true"});
+        DatabaseFactory.getDatabase().setDeviceProperties(JTangoTest.NO_DB_DEVICE_NAME, properties);
+        new DeviceProxy(NoDBDeviceManager.deviceName).command_inout("Init");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
+        final TangoAttribute attr = new TangoAttribute(NoDBDeviceManager.deviceName + "/isScheduleRunning");
+        assertThat(attr.read(boolean.class), equalTo(true));
+        properties.put("isRunRefresh", new String[]{"false"});
+        DatabaseFactory.getDatabase().setDeviceProperties(JTangoTest.NO_DB_DEVICE_NAME, properties);
+        new DeviceProxy(NoDBDeviceManager.deviceName).command_inout("Init");
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+        }
+        assertThat(attr.read(boolean.class), equalTo(false));
     }
 
 }
