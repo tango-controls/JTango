65330fe646cf08ff4be657b637a28759abeed87d
ingvord <tla-ingvord@yandex.ru>
2019-08-18 13:49:49 +0300

    do not generate backup poms

--- .travis/maven_deploy.sh
+++ .travis/maven_deploy.sh
@@ -7,7 +7,7 @@ cp .travis/settings.xml $HOME/.m2/settings.xml
 mvn deploy -Dmaven.test.skip=true
 
 cd parent
-mvn versions:set versions:update-child-modules -DprocessAllModules -DnextSnapshot
+mvn versions:set versions:update-child-modules -DprocessAllModules -DnextSnapshot -DgenerateBackupPoms=false
 cd ..
 
 .travis/push.sh
