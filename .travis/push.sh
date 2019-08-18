408965d0bfb957dc315f5c8d7efb62c26bb4784e
ingvord <tla-ingvord@yandex.ru>
2019-08-18 09:06:25 +0300

    Progress #68

---
+++ .travis/push.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "info@tango-controls.org"
+  git config --global user.name "Tango CI"
+}
+
+commit_website_files() {
+  git add . pom.xml
+  git commit --message "[Tango CI] Next development iteration"
+}
+
+upload_files() {
+  git push --quiet
+}
+
+setup_git
+commit_website_files
+upload_files
