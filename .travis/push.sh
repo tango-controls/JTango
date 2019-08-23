22239bcc259fb13faa3245c3da75918d8925adf7
Ingvord <mail@ingvord.ru>
2019-08-23 14:04:36 +0200

    PR for #68 (#78)
    build and deploy releases with github/travis/bintray

---
+++ .travis/push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "info@tango-controls.org"
+  git config --global user.name "Tango CI"
+  git remote rm origin
+  git remote add origin https://tango-ci:$GITHUB_TOKEN@github.com/tango-controls/JTango.git
+}
+
+commit_website_files() {
+  git add . pom.xml
+  git commit --message "[Tango CI] Next development iteration"
+}
+
+upload_files() {
+  git push --quiet origin HEAD:jtango-9-lts
+}
+
+setup_git
+commit_website_files
+upload_files
