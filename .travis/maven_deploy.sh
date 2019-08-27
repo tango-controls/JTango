010fa6a8620daef6f91d08fac96a3f889cc41d40
Ingvord <mail@ingvord.ru>
2019-08-27 12:04:01 +0300

    Enable sources and javadoc (#83)
    * Revert "Update .travis.yml"
    
    This reverts commit 4731fd47ed4a9f39626d6ec260d2203d457a5cc7.
    
    * enable javadoc and sources jars generation

--- .travis/maven_deploy.sh
+++ .travis/maven_deploy.sh
@@ -4,7 +4,7 @@ set -e
 
 cp .travis/settings.xml $HOME/.m2/settings.xml
 
-mvn deploy -Dmaven.test.skip=true
+mvn deploy -Dmaven.test.skip=true -Prelease
 
 cd parent
 mvn versions:set versions:update-child-modules -DprocessAllModules -DnextSnapshot -DgenerateBackupPoms=false
