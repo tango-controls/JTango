c2f2bd9ba3c864cf9262b0f68918184bafaffe05
Krystian Kedron <kedronkrystian@gmail.com>
2020-11-02 22:11:43 +0100

    Integration test for DeviceProxy/DbAttribute methods
    Test creating/removing the DBAttribute to validate conversion to List

--- server/src/test/java/org/tango/server/testserver/PropertiesTest.java
+++ server/src/test/java/org/tango/server/testserver/PropertiesTest.java
@@ -25,17 +25,19 @@
 package org.tango.server.testserver;
 
 import fr.esrf.Tango.DevFailed;
-import fr.esrf.TangoApi.ApiUtil;
-import fr.esrf.TangoApi.AttributeInfo;
-import fr.esrf.TangoApi.Database;
-import fr.esrf.TangoApi.DbDatum;
-import fr.esrf.TangoApi.DeviceData;
-import fr.esrf.TangoApi.DeviceProxy;
-import org.junit.*;
+import fr.esrf.TangoApi.*;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.tango.server.ServerManager;
 import org.tango.utils.DevFailedUtils;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -44,8 +46,7 @@ import static org.junit.Assert.assertThat;
  * @author ABEILLE
  */
 public class PropertiesTest {
-    private static String deviceName = "test/tango/jtangotest.1";
-
+    private static final String deviceName = "test/tango/jtangotest.1";
     @BeforeClass
     public static void createDeviceInTangoDB() throws DevFailed {
         System.out.println("Tango host = " + System.getProperty("TANGO_HOST"));
@@ -69,8 +70,7 @@ public class PropertiesTest {
 
             db.put_device_property(deviceName, dbDatum);
 
-            connect();
-            final DeviceProxy dev = new DeviceProxy(deviceName);
+            final DeviceProxy dev = getDeviceProxy();
             DeviceData devda = dev.command_inout("getMyProperty");
             String value = devda.extractString();
 
@@ -99,9 +99,8 @@ public class PropertiesTest {
 
         db.put_class_property(JTangoTest.class.getCanonicalName(), dbDatum);
 
-        connect();
+        final DeviceProxy dev = getDeviceProxy();
 
-        final DeviceProxy dev = new DeviceProxy(deviceName);
         DeviceData devdaClass = dev.command_inout("getMyClassProperty");
 
         String[] valueClass = devdaClass.extractStringArray();
@@ -118,11 +117,10 @@ public class PropertiesTest {
 
     @Test
     public void testAttributeProperty() throws DevFailed {
-        connect();
         final Double dd = new Double(Math.random());
         final String value = dd.toString();
         final String attrName = "shortScalar";
-        final DeviceProxy dev = new DeviceProxy(deviceName);
+        final DeviceProxy dev = getDeviceProxy();
         final AttributeInfo info = dev.get_attribute_info(attrName);
         info.description = value;
         dev.set_attribute_info(new AttributeInfo[]{info});
@@ -130,8 +128,87 @@ public class PropertiesTest {
         Assert.assertEquals(value, actualValue);
     }
 
+    @Test
+    public void testDbDynamicAttributePropertyRead() throws DevFailed {
+        int defaultDynamicAttrPropertiesSize = 2;
+        List<String> dynamicAttribute = Arrays.asList("StringDynamic",
+                "String[]Dynamic",
+                "doubleDynamic",
+                "double[]Dynamic",
+                "intDynamic",
+                "int[]Dynamic",
+                "floatDynamic",
+                "float[]Dynamic",
+                "shortDynamic",
+                "short[]Dynamic",
+                "longDynamic",
+                "long[]Dynamic",
+                "byteDynamic",
+                "byte[]Dynamic",
+                "booleanDynamic");
+
+        final DeviceProxy dev = getDeviceProxy();
+        for (String attr : dev.get_attribute_list()) {
+            if (dynamicAttribute.contains(attr)) {
+                DbAttribute dbAttr = dev.get_attribute_property(attr);
+                assertEquals(defaultDynamicAttrPropertiesSize, dbAttr.size());
+            }
+        }
+    }
+
+    @Test
+    public void testDbDynamicAttributePropertyAddAndDelete() throws DevFailed {
+        final DeviceProxy dev = getDeviceProxy();
+
+        String attributeName = "doubleScalar";
+        String testPropertyName = "testProperty";
+        String testPropertyValue = "exampleValue";
+
+        updateDbAttributeProperty(dev, attributeName, testPropertyName, testPropertyValue);
+        Assert.assertEquals(testPropertyValue,
+                dev.get_attribute_property(attributeName).get_string_value(testPropertyName));
+
+        cleanupDbAttributeProperty(dev, attributeName, testPropertyName);
+
+        String attributeName2 = "longScalar";
+        String testPropertyName2 = "testPropertyForLongScalar";
+        double testPropertyValue2 = 1.2;
+
+        DbAttribute dbAttr1 = updateDbAttributeProperty(dev, attributeName, testPropertyName, testPropertyValue);
+        DbAttribute dbAttr2 = updateDbAttributeProperty(dev, attributeName2, testPropertyName2, testPropertyValue2);
+
+        dev.put_attribute_property(new DbAttribute[] {dbAttr1, dbAttr2});
+        DbAttribute[] dbAttributes = dev.get_attribute_property(new String[] {attributeName, attributeName2});
+
+        assertEquals(2, dbAttributes.length);
+        assertEquals(testPropertyValue, dbAttributes[0].get_string_value(testPropertyName));
+        assertEquals(testPropertyValue2, dbAttributes[1].datum(testPropertyName2).extractDouble(), 0.0);
+
+        cleanupDbAttributeProperty(dev, attributeName, testPropertyName);
+        cleanupDbAttributeProperty(dev, attributeName2, testPropertyName2);
+    }
+
+    private DbAttribute updateDbAttributeProperty(DeviceProxy dev, String attributeName,
+                                                  String propName, Object propValue) throws DevFailed {
+        DbAttribute dbAttr = dev.get_attribute_property(attributeName);
+        dbAttr.add(propName, String.valueOf(propValue));
+        dev.put_attribute_property(dbAttr);
+
+        return dbAttr;
+    }
+
+    private void cleanupDbAttributeProperty(DeviceProxy dev, String attributeName, String propName) throws DevFailed {
+        dev.delete_attribute_property(attributeName, propName);
+        Assert.assertEquals(0, dev.get_attribute_property(propName).size());
+    }
+
     public void connect() throws DevFailed {
         JTangoTest.start();
+    }
+
+    public DeviceProxy getDeviceProxy() throws DevFailed {
+        connect();
+        return new DeviceProxy(deviceName);
     }
 
     @After
